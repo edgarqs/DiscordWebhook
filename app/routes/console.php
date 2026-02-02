089f937da1b76539971075fb9cf9679447270f01
Edgar QS <edgar345.eq@gmail.com>
2026-02-02 16:14:52 +0100

    feature: permite monitorización directa con UptimeKuma

--- app/routes/console.php
+++ app/routes/console.php
@@ -10,6 +10,3 @@ Artisan::command('inspire', function () {
 
 // Schedule the processing of scheduled messages every minute
 Schedule::command('scheduled-messages:process')->everyMinute();
-
-// Para UptimeKuma
-Schedule::command('monitor:push')->everyMinute();
